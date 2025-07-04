e5736c2254e79e018d6017f9d30193718fa1580d
Ruly Octareza <rulyoctareza@gmail.com>
2025-07-04 19:58:43 +0700

    Format code for improved readability

--- lib/common/ssl_pinning.dart
+++ lib/common/ssl_pinning.dart
@@ -6,7 +6,9 @@ class SslPinning {
   static Future<IOClient> createPinnedClient() async {
     final context = SecurityContext(withTrustedRoots: false);
     // Ganti path sertifikat sesuai lokasi file .cer kamu di assets
-    final certBytes = await rootBundle.load('assets/certificates/themoviedb.org.pem');
+    final certBytes = await rootBundle.load(
+      'assets/certificates/themoviedb.org.pem',
+    );
     context.setTrustedCertificatesBytes(certBytes.buffer.asUint8List());
     final httpClient = HttpClient(context: context);
     return IOClient(httpClient);
